c99eaf2a289b780e6f98a5411385cb3f09bff2c6
Luke Oliff <luke@lukeoliff.com>
2019-03-26 09:23:47 +0000

    hello api express

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/api', (req, res) => res.json({ message: 'Hello World!' }))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
